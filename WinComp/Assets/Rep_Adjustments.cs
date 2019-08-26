e21ba4596497e0571512ab9f98b7d4504e15d549
HenriqC <henriqcarvalh@gmail.com>
2019-08-26 14:56:04 +0100

    Still LineDraw

---
+++ WinComp/Assets/Rep_Adjustments.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rep_Adjustments : MonoBehaviour
+{
+    public Slider pathTimer;
+
+    public void setTimer(float timer)
+    {
+        Instantiate_target.instance.lineDrawTimer = timer;
+    }
+}
